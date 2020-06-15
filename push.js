fd1e1be94a2897e28bba10b930ab659926243040
Ayush Sinha <toayushofficial@gmail.com>
2020-06-15 15:20:47 +0530

    fix

--- push.js
+++ push.js
@@ -7,5 +7,5 @@ let vapidKeys = {
 push.setVapidDetails('mailto:ayush@patchus.in', vapidKeys.publicKey, vapidKeys.privateKey);
 
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/doynUmRe7Io:APA91bE2JeUK71bogE5nvCGTNJ8PdGaJqjpDRRVv0A2J3nTRR1jQr_QlRal-smSGs5w9ygXt59iY1vYJGevDyytPZI5QedRBpmJ_3e6kGmlBr8iKSMJul4CKd9QGuVtWRGKsnddp95Dm","expirationTime":null,"keys":{"p256dh":"BM5e4bw1b8G7SHP_z-AsCT3ZoZQqSM40rqOTy1tj1VaWjhxjSxT8dNV33ZjzXF4b_eF5wue1jixGKlfGdd-WDt8","auth":"ZpMoArJtdTX4pLP8EPP5Bg"}};
 push.sendNotification(sub)
